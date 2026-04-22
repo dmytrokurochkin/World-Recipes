018819b7ac14513013442eec7aaa41bb80cb8ebd
Btema2 <temabiill@gmail.com>
2026-04-22 10:53:51 +0200

    fix: compilation error

--- site-core/src/App.js
+++ site-core/src/App.js
@@ -1,4 +1,4 @@
-import Header from './components/header';
+import Header from './components/Header';
 import Footer from './components/footer';
 import Home from './pages/home';
 import CategoryMeals from './pages/categoryMeals';
